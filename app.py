8d0227cddd86e53fabd4ac43d9c15150330540bf
Edofficemay <64502297+Edofficemay@users.noreply.github.com>
2024-09-27 16:17:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,6 @@
-from flask import Flask, jsonify
-import pandas as pd
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    # Spécifiez le chemin du fichier CSV
-    file_path = r'C:\Users\Florian\Desktop\tEST Python\Don 383\530300383_201910_ECO.don'
-
-    # Lire le fichier CSV avec pandas
-    try:
-        df = pd.read_csv(file_path, delimiter=';')  # Changez le délimiteur si nécessaire
-
-        # Convertir les données en JSON (chaque ligne devient un dictionnaire)
-        data = df.to_dict(orient='records')
-
-        # Retourner les données sous forme de JSON
-        return jsonify({"data": data})
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return '{"data": "Hello, World!"}'
