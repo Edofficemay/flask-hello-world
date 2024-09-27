d7f32687380aa8721d5a19ac343292896090307c
Edofficemay <64502297+Edofficemay@users.noreply.github.com>
2024-09-27 20:11:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def Datas():
-
     # Spécifiez le chemin du fichier CSV
     file_path = 'C:/Users/Florian/Desktop/tEST Python/Don 383/530300383_201910_ECO.don'
    
     # Lire le fichier CSV avec pandas
-    data = pd.read_csv(file_path, delimiter=';')  # Changez le délimiteur si nécessaire
-    
-    return '{"data": "Hello, World!"}'
+    try:
+        data = pd.read_csv(file_path, delimiter=';')  # Changez le délimiteur si nécessaire
 
+        # Convertir les données en JSON
+        data_json = data.to_dict(orient='records')
+
+        # Retourner les données sous forme de JSON
+        return jsonify({"data": data_json})
+
+    except FileNotFoundError as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
