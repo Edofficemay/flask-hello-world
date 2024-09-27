df6f738a82bd8d046ace9740e1587ec5e1f74da7
Edofficemay <64502297+Edofficemay@users.noreply.github.com>
2024-09-27 19:38:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,17 +8,7 @@ def Datas():
     file_path = r'C:\Users\Florian\Desktop\tEST Python\Don 383\530300383_201910_ECO.don'
     
     # Lire le fichier CSV avec pandas
-    try:
-        df = pd.read_csv(file_path, delimiter=',')  # Changez le délimiteur si nécessaire
-
-        # Convertir les données en JSON (chaque ligne devient un dictionnaire)
-        data = df.to_dict(orient='records')
-
-        # Retourner les données sous forme de JSON
-        return jsonify({"data": data})
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        data = pd.read_csv(file_path, delimiter=';')  # Changez le délimiteur si nécessaire
     
     return '{"data": "Hello, World!"}'
 
