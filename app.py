09b5df0ee48d3f302878911fa03a30e5c993ce04
Edofficemay <64502297+Edofficemay@users.noreply.github.com>
2024-09-27 19:48:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def Datas():
     file_path = r'C:\Users\Florian\Desktop\tEST Python\Don 383\530300383_201910_ECO.don'
     
     # Lire le fichier CSV avec pandas
-        data = pd.read_csv(file_path, delimiter=';')  # Changez le délimiteur si nécessaire
+    data = pd.read_csv(file_path, delimiter=';')  # Changez le délimiteur si nécessaire
     
     return '{"data": "Hello, World!"}'
 
