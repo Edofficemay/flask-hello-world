1a05487e4d946865c3443174adf1ee8582e53bde
Edofficemay <64502297+Edofficemay@users.noreply.github.com>
2024-09-27 20:04:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 def Datas():
 
     # Spécifiez le chemin du fichier CSV
-    file_path = r'C:\Users\Florian\Desktop\tEST Python\Don 383\530300383_201910_ECO.don'
-    
+    file_path = 'C:/Users/Florian/Desktop/tEST Python/Don 383/530300383_201910_ECO.don'
+   
     # Lire le fichier CSV avec pandas
     data = pd.read_csv(file_path, delimiter=';')  # Changez le délimiteur si nécessaire
     
